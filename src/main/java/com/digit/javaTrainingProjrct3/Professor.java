bf53651692320f9f83805ec2cf0e01f352db6873
Suraj.Singh <Suraj.Singh@LDIGITBGL--L0263.godigit.corp>
2023-07-21 16:23:49 +0530

    new Changes

--- src/main/java/com/digit/javaTrainingProjrct3/Professor.java
+++ src/main/java/com/digit/javaTrainingProjrct3/Professor.java
@@ -2,4 +2,25 @@ package com.digit.javaTrainingProjrct3;
 
 public class Professor {
 
+	String name;
+	int id;
+	String course;
+	
+	 Professor(String name, int id, String course) {
+		this.name=name;
+		this.id=id;
+		this.course=course;
+	}
+	 
+	 
+	
+public static void main(String[] args) {
+	Professor p1=new Professor("Jarasarri", 19023, "Java");
+	Professor p2=new Professor("Arun Krishanan", 19045, "Python");
+	Professor p3=new Professor("Bhemesh", 19085, "CPP");
+	
+	
+
+	
+}
 }
