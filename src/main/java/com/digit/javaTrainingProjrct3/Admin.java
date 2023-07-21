24b9dd5684144ba4d4e1b56dd1f0b2de8eae694f
Himanshu.Kumar01 <Himanshu.Kumar01@LDIGITBGL-L0272.godigit.corp>
2023-07-21 16:29:52 +0530

    Project 3

--- src/main/java/com/digit/javaTrainingProjrct3/Admin.java
+++ src/main/java/com/digit/javaTrainingProjrct3/Admin.java
@@ -1,5 +1,15 @@
 package com.digit.javaTrainingProjrct3;
 
+
+
 public class Admin {
+	void admin() {
+		
+	}
+	public static void main(String[] args) {
+		String username;
+		String password;
+	
+	}
 
 }
